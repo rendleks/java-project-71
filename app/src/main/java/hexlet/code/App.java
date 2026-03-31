ab14732bfb885d4cc15ab5c15514808192be885c
rendleks <0rendl050@gmail.com>
2026-03-31 22:57:38 +0300

    picocli add option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,10 +2,17 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
 description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+
+    @Option(names = {"-f", "--format"}, description = "output foramt [default: stylish")
+    private String format = "stylish";
+
+
 
     @Override
     public void run() {
