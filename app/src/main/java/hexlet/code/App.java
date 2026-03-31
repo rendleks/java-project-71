50b5186ced703498ec7f3c817ffcac410763b494
rendleks <0rendl050@gmail.com>
2026-04-01 00:09:00 +0300

    Add cli description

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,16 +3,27 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
 description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
-    @Option(names = {"-f", "--format"}, description = "output foramt [default: stylish")
-    private String format = "stylish";
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            defaultValue = "stylish")
+    private String format;
 
+    @Parameters(index = "0",
+            description = "path to first file")
+    private File filepath1;
 
+    @Parameters(index = "1",
+            description = "path to second file")
+    private File filepath2;
 
     @Override
     public void run() {
