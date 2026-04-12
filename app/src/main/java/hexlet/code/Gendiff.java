7d1e791d362ee308e5c737ff5415c59c26a72118
rendleks <0rendl050@gmail.com>
2026-04-12 22:03:11 +0300

    add files and readfiles

---
+++ app/src/main/java/hexlet/code/Gendiff.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Gendiff {
+    public static Path getFilePath(String filename) {
+        Path path = Paths.get(filename);
+        Path absolutePath = path.toAbsolutePath();
+
+        return absolutePath.normalize();
+    }
+
+    public static String readFile(String filename) throws Exception {
+        var path = getFilePath(filename);
+        return Files.readString(path).trim();
+    }
+}
