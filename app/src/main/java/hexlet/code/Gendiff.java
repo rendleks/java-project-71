479d489a3130f45831bb556784e897715eb13b0e
rendleks <0rendl050@gmail.com>
2026-04-14 22:00:21 +0300

    Add parse json

--- app/src/main/java/hexlet/code/Gendiff.java
+++ app/src/main/java/hexlet/code/Gendiff.java
@@ -3,7 +3,9 @@ package hexlet.code;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hexlet.code.Json;
@@ -23,12 +25,14 @@ public class Gendiff {
         return Files.readString(path).trim();
     }
 
-    public static Json jsonToMap(String filename) {
+    public static Map<String, Object> jsonToMap(String filename) throws Exception {
         final ObjectMapper mapper = new ObjectMapper();
 
-        Json myValue = mapper.readValue(readFile(filename), Json.class);
+        String file = readFile(filename);
 
-        return myValue;
+        Map<String, Object> map = mapper.readValue(file, new TypeReference<Map<String, Object>>() {
+        });
 
+        return map;
     }
 }
